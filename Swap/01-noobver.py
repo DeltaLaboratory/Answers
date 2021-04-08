119fa87ba79b23ce648e64b9483b69acc3943e42
Delta Lab <delta@deltalab.dev>
2021-04-08 20:25:29 +0900

    Create 01-noobver.py

---
+++ Swap/01-noobver.py
@@ -0,0 +1,19 @@
+a = int(input())
+c = int(input())
+p = a + c
+m = a * c
+
+if p > m:
+    print("Result = ", (p - m))
+    if p - m < 8:
+        print("Swap!!")
+        print("a =", c, " b =", a)
+    else:
+        print("a =", a, " b =", c)
+if p < m:
+    print("Result =", (m - p))
+    if m - p < 8:
+        print("Swap!!")
+        print("a =", c, " b =", a)
+    else:
+        print("a =", a, " b =", c)
